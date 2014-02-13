638444c0b9cc455b5076ce8331aa06078d073dfe
Tobias Kräntzer <tobias@nxtbgthng.com>
2014-02-13 11:33:10 +0100

    Initial Project Setup

---
+++ ModelTransformer/ModelTransformer/ModelTransformer.h
@@ -0,0 +1,13 @@
+//
+//  ModelTransformer.h
+//  ModelTransformer
+//
+//  Created by Tobias Kräntzer on 13.02.14.
+//  Copyright (c) 2014 Tobias Kräntzer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ModelTransformer : NSObject
+
+@end
---
+++ ModelTransformer/ModelTransformer/ModelTransformer.m
@@ -0,0 +1,13 @@
+//
+//  ModelTransformer.m
+//  ModelTransformer
+//
+//  Created by Tobias Kräntzer on 13.02.14.
+//  Copyright (c) 2014 Tobias Kräntzer. All rights reserved.
+//
+
+#import "ModelTransformer.h"
+
+@implementation ModelTransformer
+
+@end
